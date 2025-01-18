57698128dc00a43a984b925c1d91c57d166a2ff4
Prakhar Varshney <prakharvarshney95@gmail.com>
2025-01-18 17:39:53 +0530

    Till 16th Jan - again

--- src/test/java/com/prakharvarshney95/base/BaseTest.java
+++ src/test/java/com/prakharvarshney95/base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.prakharvarshney95.base;
 
 import com.prakharvarshney95.driver.DriverManager;
+import com.prakharvarshney95.tests.vwoTestcases.TestVWOLogin_06_Improved_POM_PropertyReader_DriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
